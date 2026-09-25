4dadd4ced5164204f724b73f603ed0c77681ee92
Anderss0nn <Anderss0n544@outlook.com>
2026-09-26 01:03:01 +0200

    Delete math-trainer directory

--- math-trainer/js/modules/mult/mult-data.js
+++
@@ -1,7 +0,0 @@
-const MultData = {
-  getRandomFactors(max = 12) {
-    const a = Math.floor(Math.random() * max) + 1;
-    const b = Math.floor(Math.random() * max) + 1;
-    return { a, b, result: a * b };
-  }
-};
